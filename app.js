76d22ed81d2d25c637edf7ded4d4f0856d4804b4
Pierre <pierre.lam91@gmail.com>
2019-08-31 19:36:27 +0200

    [ FEATURE ] Made use of global promises for mongoose and initialized a connection to db

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber');
 
 app.use(bodyParser.json());
 routes(app);
