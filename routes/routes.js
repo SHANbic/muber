20aca58cf7d80c3aa138894693f0a288d7794454
Pierre <pierre.lam91@gmail.com>
2019-08-31 18:19:57 +0200

    [ FEATURE ] Added a new route to handle post request

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
+const DriversController = require('../controllers/drivers_controller');
 
-const DriversController = require('../controllers/drivers_controller')
 module.exports = app => {
-  app.get('/api', DriversController.greeting)
+  app.get('/api', DriversController.greeting);
+
+  app.post('/api/drivers', DriversController.create);
 };
